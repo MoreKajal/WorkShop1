ea544860d918b922e56ee14341a3c11bb4bafe0d
MoreKajal <kajalm3105@gmail.com>
2021-02-13 10:02:49 -0800

    [Kajal]Added:Code to create CrossBoard

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,13 @@
 public class TicTacToe
 {
+	public static void createCrossBoard(){
+		char[] crossBoard = new char[10];
+		for (int i = 0; i < crossBoard.length; i++){
+			crossBoard[i] = ' ';
+		}
+	}
         public static void main(String args[]) {
                 System.out.println("***Welcome to Tic Tac Toe Game***");
+		createCrossBoard();
 	}
 }
